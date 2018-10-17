353d72fd5364fd5c36a33f1a702b9c8e83689920
Jacey Tupa and Noah Schooler <jacey@>
2018-10-17 13:44:13 -0700

    add front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,44 @@
-$(document).ready(function() {
-  var add = function(number1, number2){
-  return number1 + number2;
-  };
-  var number1 = parseInt(prompt("enter a number:"));
-  var number2 = parseInt(prompt("enter another number"));
-  alert(add(number1, number2));
+// Back-end logic:
+var add = function(number1, number2, number3) {
 
-  var multiply = function(number1, number2){
+  return number1 + number2 + number3;
+};
+
+var multiply = function(number1, number2) {
   return number1 * number2;
-  };
-  var number1 = parseInt(prompt("enter a number:"));
-  var number2 = parseInt(prompt("enter another number"));
-  alert(multiply(number1, number2));
+};
 
-  var subtract = function(number1, number2){
+var subtract = function(number1, number2) {
   return number1 - number2;
-  };
-  var number1 = parseInt(prompt("enter a number:"));
-  var number2 = parseInt(prompt("enter another number"));
-  alert(subtract(number1, number2));
+};
 
-  var divide = function(number1, number2){
+var divide = function(number1, number2) {
   return number1 / number2;
-  };
-  var number1 = parseInt(prompt("enter a number:"));
-  var number2 = parseInt(prompt("enter another number"));
-  var result = divide(number1, number2);
-  alert(result);
+};
+
+// Front-end logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var number3 = parseInt($("#add3").val());
+
+    var result = add(number1, number2, number3);
+
+    $("#output").text(result);
+  });
 });
+
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number:"));
+  // alert(multiply(number1, number2));
+  //
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number:"));
+  // alert(subtract(number1, number2));
+  //
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number:"));
+  // var result = divide(number1, number2);
+  // alert(result);
